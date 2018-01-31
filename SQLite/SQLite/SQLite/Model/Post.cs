81709afc3bbcdbdafaf40006944c9b3962642331
cauris <cauris@peruapps.com.pe>
2018-01-31 17:03:11 -0500

    actualizacion

---
+++ SQLite/SQLite/SQLite/Model/Post.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace SQLite.Model
+{
+    public class Post
+    {
+        [PrimaryKey,AuotIncrement]
+        public int Id { get; set; }
+        public string Experience { get; set; }
+    }
+}
